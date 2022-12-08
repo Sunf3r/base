e8dd83a0fb0baa5be83597fe32307812ada68448
Sunf3r <sunf3r@proton.me>
2022-12-08 03:42:09 -0300

    fix workers error in windows and change some things

--- Plugins/Prototypes.ts
+++ Plugins/Prototypes.ts
@@ -4,27 +4,21 @@ import type * as _Types from "../@types/index.d.ts";
 import { DateTime } from "@luxon";
 import colors from "@colors";
 
-declare global { // criando variável global 'services'
+declare global { // declarando variáveis globais
     const services: Services;
+
     interface Window {
         services: unknown;
     }
 }
 
-window.services = {};
-
 export default () => {
     colors; // Carregando o arquivo da lib (essa função é executada sozinha)
+    const spin = log("main", "Configurando prototypes...");
 
-    const spin = logSpin("main", "Configurando prototypes...");
-    /* Number Prototypes */
-
-    //      num.toMB ==> Byte to MB
-    Object.defineProperty(Number.prototype, "toMB", {
-        value: function () {
-            return (this / 1024 / 1024).toFixed(2) + "MB";
-        },
-    });
+    // Eu criei essa variável pra facilitar o
+    // gerenciamento de threads/workers
+    window.services = {};
 
     /* String Prototypes */
 
@@ -32,10 +26,11 @@ export default () => {
     Object.defineProperties(String.prototype, {
         "toPascalCase": {
             value: function () { // 'deeno forever'
-                return this.split(" ").map((word: string) =>
-                    // ['deeno', 'forever]
-                    word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase() // ['Deeno', 'Forever']
-                ).join(" "); // 'Deeno Forever'
+                return this.split(" ") // ['deeno', 'forever]
+                    .map((word: string) =>
+                        word.slice(0, 1).toUpperCase() + // D
+                        word.slice(1).toLowerCase() // eeno
+                    ).join(" "); // 'Deeno Forever'
             },
         },
         "toSector": {
@@ -45,47 +40,57 @@ export default () => {
         },
     });
 
-    //      console.error()
+    /* Number Prototypes */
+
+    //      num.toMB ==> Byte to MB
+    Object.defineProperty(Number.prototype, "toMB", {
+        value: function () {
+            return (this / 1024 / 1024).toFixed(2) + "MB";
+        },
+    });
+
+    /*      console.error        */
+
     // maneira mais simples e prática de criar bons logs de erros
     console.error = (error: { stack: string }, title: string) => {
-        const msg = String(error?.stack || error);
-        const spin = logSpin((title || "ERROR").red, msg);
+        const msg = String(error?.stack || error)
+            .slice(0, 512).red;
 
-        spin.fail(msg.slice(0, 512).red);
+        log((title || "ERROR").red, msg)
+            .fail(msg);
     };
 
     spin.end("Prototypes configurados.");
 };
-export { logSpin, now };
 
-const logSpin = (sector: string, text: string) => {
-    const sp = new TerminalSpinner({
+export { log, now };
+
+const log = (sector: string, text: string) => {
+    const spinner = new TerminalSpinner({
         text: formatLog(sector, text),
         spinner: SpinnerTypes.arc,
         color: "red",
-    })
-        .start();
+    }).start();
 
     return {
-        end: (msg: string) => sp.succeed(formatLog(sector, msg)),
-        fail: (msg: string) => sp.fail(formatLog(sector, msg)),
+        end: (msg: string) => spinner.succeed(formatLog(sector, msg)),
+        fail: (msg: string) => spinner.fail(formatLog(sector, msg)),
     };
 };
 
-function formatLog(sector: string, text: string) {
-    return [ // isso pode parecer meio confuso
+const formatLog = (sector: string, text: string) =>
+    [ // isso pode parecer meio confuso
         "[",
-        sector.toUpperCase().blue,
+        sector.toUpperCase().blue, // [ SECTOR
         "|",
-        now().toFormat("T").yellow,
-        "|", // mas é só a configuração
-        ((Deno.memoryUsage().rss / 1048576).toFixed(2) + "MB").green,
+        now().toFormat("T").yellow, // [ SECTOR | 18:04
+        "|",
+        ((Deno.memoryUsage().rss / 1024 / 1024).toFixed(2) + "MB").green, // [ SECTOR | 18:04 | 69MB
         "] -",
-        text, // do console.log
+        text,
     ].join(" "); // [ SETOR | 18:04 | 69MB ]
-}
 
 const now = () =>
     DateTime.now()
-        .setZone(config.date.timezone) // hora com o fuso-horário corrigido
+        .setZone(config.date.timezone) // horário com o fuso-horário corrigido
         .setLocale(config.date.locale); //definindo o idioma da formatação de datas
